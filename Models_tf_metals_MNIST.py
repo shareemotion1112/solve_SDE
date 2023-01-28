1496512b81a736fae8f96a062a63c3cba764d17d
shareemotion1112 <shareemotion1112@gmail.com>
2023-01-28 23:37:33 +0900

    bug fix: time embedding...

--- Models_tf_metals_MNIST.py
+++ Models_tf_metals_MNIST.py
@@ -17,23 +17,22 @@ import copy
 # input : [batch, in_height, in_width, in_channels] 형식. 28x28x1 형식의 손글씨 이미지.
 # filter : [filter_height, filter_width, in_channels, out_channels] 형식. 3, 3, 1, 32의 w.
 import os
-from PIL import Image
-
 
 
 SIGMA = 25.
 IS_TRAIN_MODEL = True
 IS_SAVEFIG = False
 BATCH_SIZE = 32
-NUM_STEPS = 200
+NUM_STEPS = 500
 EPS = 1e-3
-LEARNING_RATE = 1e-3
+LEARNING_RATE = 1e-2
 SNR = 0.16
 output_dim = 1
 epochs = 20
+n_images = 60000
 
 data = tf.keras.datasets.mnist.load_data(path="mnist.npz")
-images = data[0][0]
+images = data[0][0][:n_images]
 
 class ImageDataset:
     def __init__(self, images, batch_size=BATCH_SIZE, isResize=True):
@@ -57,7 +56,7 @@ class ImageDataset:
 dataset = ImageDataset(images, batch_size=BATCH_SIZE)
 
 # check dataset
-x = dataset[0]
+# x = dataset[0]
 # for i in range(x.shape[0]):
 #     plt.imshow(x[i, :, :])
 #     plt.show(block=False)
@@ -82,13 +81,15 @@ def generate_random(shape, min = None, max = None, type = tf.float32):
     else:
         return tf.random.uniform(shape, minval=min, maxval=max, dtype=type, seed=seed)
 
-def GaussianFourierProjection(x, embed_dim=256, scale=30):
+def GaussianFourierProjection(t, embed_dim=256, scale=30):
     # tensorflow에서는 Weight, bias를 임의로 변경하는 것이 어려운 듯
     # keras 패키지의 미리 만들어진 Weight를 사용    
     
     W = generate_random([embed_dim // 2])
-    x_proj = 2.0 * np.pi * scale * x
+    x_proj = 2.0 * np.pi * scale * t
     x_proj = x_proj[:, None] * W[None, :]
+    a = (x_proj[0, 1])
+    b = (x_proj[0, 2])
     x_proj_sin = tf.sin(x_proj)
     x_proj_cos = tf.cos(x_proj)
 
@@ -102,12 +103,11 @@ def marginal_prob_std(t, sigma = SIGMA):
 
 def loss_fn(model, x, marginal_prob_std, eps=1e-5): # ------------------ random t 를 사용??
     random_t = generate_random([BATCH_SIZE]) * (1. - eps) + eps
-    z = generate_random(x.shape, 0, 255, tf.int32)
+    z = generate_random(x.shape) # z : 0 ~ 255까지의 크기인줄 알았는데 0~1사이 숫자임
     z = tf.cast(z, dtype=tf.float32)
     std = marginal_prob_std(random_t)
     perturbed_x = x + z * std[:, None, None]
     score = model(perturbed_x, random_t)
-    # print(f"score dimension : {score.shape}")
     sum = tf.reduce_sum((score * std + z[:, :, :, None])**2, axis=(1, 2, 3))
 
     loss = tf.reduce_mean(sum)
@@ -197,7 +197,7 @@ def ScoreNet2D(x, t, channels=[32, 64, 128, 256]):
 if IS_TRAIN_MODEL is True:
     # train scoreNet
     train_loss = tf.keras.metrics.Mean()
-    random_t = generate_random([BATCH_SIZE])
+    random_t = tf.ones(BATCH_SIZE) * generate_random([]) # 같은 값으로 된 배열이어야 함
 
     x = keras.Input((56, 56, 1))
     y = ScoreNet2D(x, random_t)
@@ -244,11 +244,9 @@ if IS_TRAIN_MODEL is True:
         plt.title('score')
         plt.subplots_adjust(hspace=0.5)
         plt.show(block=False)
-        plt.pause(1)
+        plt.pause(0.1)
         plt.close()
-
-
-if IS_TRAIN_MODEL is False:
+else:
     # # model load
     files = os.listdir()    
     import re
@@ -269,7 +267,17 @@ if IS_TRAIN_MODEL is False:
     
 
 
-
+def plot_imgs(x):
+    n_images = x.shape[0]
+    n_row = int(np.ceil(np.sqrt(n_images)))
+    n_col = int(n_row + 1)
+    for i in range(1, n_images):
+        plt.subplot(n_row, n_col, i)
+        plt.imshow(x[i, :, :, :])        
+        plt.axis('off')    
+    plt.subplots_adjust(hspace=0)
+    plt.show(block=False)
+            
 
 
 class VE_SDE:
@@ -280,7 +288,7 @@ class VE_SDE:
     def diffusion_coef(self, t, sigma=SIGMA):
         return sigma ** t
     
-    def run_pc_sampler(self, x, num_steps = NUM_STEPS, eps=EPS, snr = SNR):
+    def run_pc_sampler(self, x, num_steps = NUM_STEPS, eps=EPS, snr=SNR):
         time_steps = np.linspace(1., eps, num_steps)
         step_size = time_steps[0] - time_steps[1]
         
@@ -289,74 +297,60 @@ class VE_SDE:
 
             # corrector step (Langevin MCMC)
             grad = self.scoreNet(x, batch_time_step)
-            a = tf.reshape(grad, (grad.shape[0], -1))
-            b = tf.norm(tf.reshape(grad, (grad.shape[0], -1)), axis=0)
             grad_norm = tf.math.reduce_mean(tf.norm(tf.reshape(grad, (grad.shape[0], -1)), axis=-1))
             noise_norm = np.sqrt(np.prod(x.shape[1:])) # 400
             langevin_step_size = 2 * (snr * noise_norm / grad_norm)**2
-            x = x + langevin_step_size * grad + tf.sqrt(2*langevin_step_size) * generate_random([x.shape])
+            x = x + langevin_step_size * grad + tf.sqrt(2*langevin_step_size) * generate_random(x.shape)
             x_corrector = tf.identity(x)
             # predictor step (Euler -Maruyama)
             g = self.diffusion_coef(batch_time_step)
             x_mean = x + (g**2)[:, None, None, None] * scorenet(x, batch_time_step) * step_size
-            x = x_mean + tf.sqrt(g**2 * step_size)[:, None, None, None] * generate_random([x.shape])
-            plt.subplot(1, 2, 1)
-            plt.imshow(x_corrector[0, :, :, :])
-            plt.title('corrector')
-            plt.subplot(1, 2, 2)
-            plt.imshow(x[0, :, :, :])
-            plt.title('predictor')
-            plt.show(block=False)
-            plt.pause(0.1)
-            plt.close()
-        # The last step does not include any noise !!!!!!
+            x = x_mean + tf.sqrt(g**2 * step_size)[:, None, None, None] * generate_random(x.shape)
+            if i % 10 == 0:
+                plot_imgs(x)
+                plt.pause(0.1)
+                plt.close()            
+        # 마지막 step에서는 noise를 제거한 녀석을 return
         return x_mean
 
 
 ve_model = VE_SDE(BATCH_SIZE, scorenet)
-# for x, y in data_loader:
-#     denoising_x = ve_model.run_denoising(x)
-#     pp("denoising x : {denoising_x.shape}")
-#     plot(x, scoreNet(x, 1), denoising_x)
-    # predictor_x = ve_model.run_predictor_only(x)
-    # plot(x, scoreNet(x, 1), predictor_x, denoising_x)
 
-# # random matrix check : 아예 random한 데이터는 어려운 듯    
-# x = torch.abs(torch.randn((1, 1, 400, 400)))
-# for i in range(1000):
-#     denoised_x = ve_model.run_denoising(x)        
-#     x = denoised_x
-#     plot(scoreNet(x, 1), denoised_x)
-# import matplotlib.pyplot as plt
-# plt.imshow(denoised_x[0, 0, :, :].cpu().detach().numpy(), cmap='gray')
-# plt.show()
 
-original_path = os.getcwd()
-os.chdir(original_path + "/results")
+t = tf.ones(BATCH_SIZE) # initial time이라 1을 넣는가봄
+std = marginal_prob_std(t)[:, None, None, None]
+x = tf.random.uniform((32, 56, 56, 1), seed=np.random.randint(0, 10000)) * std
+
+denoised_x = ve_model.run_pc_sampler(x)
+plot_imgs(denoised_x)
+
+
+
 # 데이터의 가운데를 지우고 테스트 
+# original_path = os.getcwd()
+# os.chdir(original_path + "/results")
+# offset = 10
+# for x in dataset:
+#     x_cp = copy.copy(x)
+#     x_cp[:, (56-offset):(56+offset), (56-offset):(56+offset)] = 0
+#     denoising_x = ve_model.run_pc_sampler(x_cp[:, :, :, None])
 
-offset = 10
-for x in dataset:
-    x_cp = copy.copy(x)
-    x_cp[:, (56-offset):(56+offset), (56-offset):(56+offset)] = 0
-    denoising_x = ve_model.run_pc_sampler(x_cp[:, :, :, None])
+#     image_name = str(math.ceil(time.time())) + '.png'
 
-    image_name = str(math.ceil(time.time())) + '.png'
+#     plt.subplot(1, 3, 1)
+#     plt.imshow(x[0, :, :])
+#     plt.title('original')
+#     plt.subplot(1, 3, 2)
+#     plt.imshow(x_cp[0, :, :])
+#     plt.title('cropped')
+#     plt.subplot(1, 3, 3)
+#     plt.imshow(denoising_x[0, :, :])
+#     plt.title('denoised')
+#     plt.subplots_adjust(hspace=0.5)
+#     plt.show(block=False)    
+#     if IS_SAVEFIG is True:
+#         plt.savefig(image_name)
+#     plt.pause(1)
+#     plt.close()
 
-    plt.subplot(1, 3, 1)
-    plt.imshow(x[0, :, :])
-    plt.title('original')
-    plt.subplot(1, 3, 2)
-    plt.imshow(x_cp[0, :, :])
-    plt.title('cropped')
-    plt.subplot(1, 3, 3)
-    plt.imshow(denoising_x[0, :, :])
-    plt.title('denoised')
-    plt.subplots_adjust(hspace=0.5)
-    plt.show(block=False)    
-    if IS_SAVEFIG is True:
-        plt.savefig(image_name)
-    plt.pause(1)
-    plt.close()
-
-os.chdir(original_path)
+# os.chdir(original_path)
